72ccf0a348dc9cf05c4052e26cda272457b0d3c0
tungnt <nguyenthanhtung74119@gmail.com>
2022-06-06 00:17:45 +0700

    tungt

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,173 @@
 <?php
-// Add custom Theme Functions here
+#Disable Self Pingbacks in WordPress Using Plugins
+function no_self_ping( &$links ) {
+	$home = get_option( 'home' );
+	foreach ( $links as $l => $link )
+			if ( 0 === strpos( $link, $home ) )
+					unset($links[$l]);
+}
+
+add_action( 'pre_ping', 'no_self_ping' );
+add_filter('xmlrpc_enabled', '__return_false');
+#!Disable Self Pingbacks in WordPress Using Plugins
+/***
+  * DISABLE EMOJIS IN WORDPRESS
+*/
+  function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );	
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	// Remove from TinyMCE
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emoji_feature() {
+	// Prevent Emoji from loading on the front-end
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	// Remove from admin area also
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	// Remove from RSS feeds also
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji');
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji');
+	// Remove from Embeds
+	remove_filter( 'embed_head', 'print_emoji_detection_script' );
+	// Remove from emails
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	// Disable from TinyMCE editor. Currently disabled in block editor by default
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	/** Finally, prevent character conversion too
+         ** without this, emojis still work 
+         ** if it is available on the user's device
+	 */
+	add_filter( 'option_use_smilies', '__return_false' );
+}
+function disable_emojis_tinymce( $plugins ) {
+	if( is_array($plugins) ) {
+		$plugins = array_diff( $plugins, array( 'wpemoji' ) );
+	}
+	return $plugins;
+}
+add_action('init', 'disable_emoji_feature');
+add_filter( 'option_use_smilies', '__return_false' );
+/***
+  * !DISABLE EMOJIS IN WORDPRESS
+*/
+/***
+ * Brand
+ */
+function cptui_register_my_taxes_thuong_hieu() {
+
+	/**
+	 * Taxonomy: Thương Hiệu.
+	 */
+
+	$labels = [
+		"name" => __( "Thương Hiệu", "custom-post-type-ui" ),
+		"singular_name" => __( "Thương Hiệu", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => __( "Thương Hiệu", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'thuong_hieu', 'with_front' => true, ],
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"show_tagcloud" => true,
+		"rest_base" => "thuong_hieu",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"rest_namespace" => "wp/v2",
+		"show_in_quick_edit" => false,
+		"sort" => true,
+		"show_in_graphql" => false,
+	];
+	register_taxonomy( "thuong_hieu", [ "product" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_thuong_hieu' );
+
+/***
+ * ! Brand
+ */
+
+/*
+Code Purpose : Remove woocommerce product-category slug
+*/
+add_filter('request', function( $vars ) {
+	global $wpdb;
+	if( ! empty( $vars['pagename'] ) || ! empty( $vars['category_name'] ) || ! empty( $vars['name'] ) || ! empty( $vars['attachment'] ) ) {
+		$slug = ! empty( $vars['pagename'] ) ? $vars['pagename'] : ( ! empty( $vars['name'] ) ? $vars['name'] : ( !empty( $vars['category_name'] ) ? $vars['category_name'] : $vars['attachment'] ) );
+		$exists = $wpdb->get_var( $wpdb->prepare( "SELECT t.term_id FROM $wpdb->terms t LEFT JOIN $wpdb->term_taxonomy tt ON tt.term_id = t.term_id WHERE tt.taxonomy = 'product_cat' AND t.slug = %s" ,array( $slug )));
+		if( $exists ){
+			$old_vars = $vars;
+			$vars = array('product_cat' => $slug );
+			if ( !empty( $old_vars['paged'] ) || !empty( $old_vars['page'] ) )
+				$vars['paged'] = ! empty( $old_vars['paged'] ) ? $old_vars['paged'] : $old_vars['page'];
+			if ( !empty( $old_vars['orderby'] ) )
+	 	        	$vars['orderby'] = $old_vars['orderby'];
+      			if ( !empty( $old_vars['order'] ) )
+ 			        $vars['order'] = $old_vars['order'];	
+		}
+	}
+	return $vars;
+});
+ 
+add_filter('term_link', 'term_link_filter', 10, 3);
+function term_link_filter( $url, $term, $taxonomy ) {
+    $url=str_replace("/./","/",$url);
+     return $url;
+}
+/*
+! Code Purpose : Remove woocommerce product-category slug
+*/
+function cptui_register_my_taxes() {
+
+	/**
+	 * Taxonomy: Thương Hiệu.
+	 */
+
+	$labels = [
+		"name" => __( "Thương Hiệu", "custom-post-type-ui" ),
+		"singular_name" => __( "Thương Hiệu", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => __( "Thương Hiệu", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'thuong_hieu', 'with_front' => true, ],
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"show_tagcloud" => true,
+		"rest_base" => "thuong_hieu",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"rest_namespace" => "wp/v2",
+		"show_in_quick_edit" => false,
+		"sort" => true,
+		"show_in_graphql" => false,
+	];
+	register_taxonomy( "thuong_hieu", [ "product" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes' );
