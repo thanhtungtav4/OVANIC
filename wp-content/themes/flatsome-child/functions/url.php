cf6244bc92c292e3a4335718eacd75363d7a9a39
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-06-21 11:44:05 +0700

    tungnt

--- wp-content/themes/flatsome-child/functions/url.php
+++ wp-content/themes/flatsome-child/functions/url.php
@@ -1,5 +1,96 @@
 <?php
 /*
+* Remove thuong_hieu in URL
+* Thay thuong_hieu bằng slug hiện tại của bạn. Mặc định là thuong_hieu
+*/
+add_filter( 'term_link', 'devvn_product_thuong_hieu_permalink', 10, 3 );
+function devvn_product_thuong_hieu_permalink( $url, $term, $taxonomy ){
+    switch ($taxonomy):
+        case 'thuong-hieu':
+              $taxonomy_slug = 'thuong-hieu'; //Thay bằng slug hiện tại của bạn. Mặc định là thuong-hieu
+              if(strpos($url, $taxonomy_slug) === FALSE) break;
+              $url = str_replace('/' . $taxonomy_slug, '', $url);
+              break;
+    endswitch;
+    return $url;
+}
+// Add our custom thuong-hieu rewrite rules
+function devvn_thuong_hieu_rewrite_rules($flash = false) {
+  $terms = get_terms( array(
+      'taxonomy' => 'thuong-hieu',
+      'post_type' => 'product',
+      'hide_empty' => false,
+  ));
+  if($terms && !is_wp_error($terms)){
+      $siteurl = esc_url(home_url('/'));
+      foreach ($terms as $term){
+          $term_slug = $term->slug;
+          $baseterm = str_replace($siteurl,'',get_term_link($term->term_id,'thuong-hieu'));
+          add_rewrite_rule($baseterm.'?$','index.php?thuong-hieu='.$term_slug,'top');
+          add_rewrite_rule($baseterm.'page/([0-9]{1,})/?$', 'index.php?thuong-hieu='.$term_slug.'&paged=$matches[1]','top');
+          add_rewrite_rule($baseterm.'(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?thuong-hieu='.$term_slug.'&feed=$matches[1]','top');
+      }
+  }
+  if ($flash == true)
+      flush_rewrite_rules(false);
+}
+add_action('init', 'devvn_thuong_hieu_rewrite_rules');
+/*Sửa lỗi khi tạo mới taxomony bị 404*/
+add_action( 'create_term', 'new_thuong_hieu_cat_edit_success', 10, 2 );
+function new_thuong_hieu_cat_edit_success( $term_id, $taxonomy ) {
+    devvn_thuong_hieu_rewrite_rules(true);
+}
+
+/*
+* ! Remove thuong_hieu in URL
+* Thay thuong_hieu bằng slug hiện tại của bạn. Mặc định là thuong_hieu
+*/
+
+/***
+ * Xóa bỏ product-category và toàn bộ slug của danh mục cha khỏi đường dẫn của Woocommerce
+ */
+// Add our custom product cat rewrite rules
+add_filter('term_link', 'devvn_no_term_parents', 1000, 3);
+function devvn_no_term_parents($url, $term, $taxonomy) {
+    if($taxonomy == 'product_cat'){
+        $term_nicename = $term->slug;
+        $url = trailingslashit(get_option( 'home' )) . user_trailingslashit( $term_nicename, 'category' );
+    }
+    return $url;
+}
+
+// Add our custom product cat rewrite rules
+function devvn_no_product_cat_parents_rewrite_rules($flash = false) {
+    $terms = get_terms( array(
+        'taxonomy' => 'product_cat',
+        'post_type' => 'product',
+        'hide_empty' => false,
+    ));
+    if($terms && !is_wp_error($terms)){
+        foreach ($terms as $term){
+            $term_slug = $term->slug;
+            add_rewrite_rule($term_slug.'/?$', 'index.php?product_cat='.$term_slug,'top');
+            add_rewrite_rule($term_slug.'/page/([0-9]{1,})/?$', 'index.php?product_cat='.$term_slug.'&paged=$matches[1]','top');
+            add_rewrite_rule($term_slug.'/(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?product_cat='.$term_slug.'&feed=$matches[1]','top');
+        }
+    }
+    if ($flash == true)
+        flush_rewrite_rules(false);
+}
+add_action('init', 'devvn_no_product_cat_parents_rewrite_rules');
+
+/*Sửa lỗi khi tạo mới taxomony bị 404*/
+add_action( 'create_term', 'devvn_new_product_cat_edit_success', 10);
+add_action( 'edit_terms', 'devvn_new_product_cat_edit_success', 10);
+add_action( 'delete_term', 'devvn_new_product_cat_edit_success', 10);
+function devvn_new_product_cat_edit_success( ) {
+    devvn_no_product_cat_parents_rewrite_rules(true);
+}
+/***
+ * Xóa bỏ product-category và toàn bộ slug của danh mục cha khỏi đường dẫn của Woocommerce
+ */
+
+/*
 * Code Bỏ /product/ hoặc /cua-hang/ hoặc /shop/ ... có hỗ trợ dạng %product_cat%
 * Thay /cua-hang/ bằng slug hiện tại của bạn
 */
@@ -52,73 +143,3 @@ function devvn_woo_new_product_post_save($post_id){
   }
 }
 add_action('wp_insert_post', 'devvn_woo_new_product_post_save');
-
-/*
-* Remove product-category in URL
-* Thay product-category bằng slug hiện tại của bạn. Mặc định là product-category
-*/
-add_filter( 'term_link', 'devvn_product_cat_permalink', 10, 3 );
-function devvn_product_cat_permalink( $url, $term, $taxonomy ){
-    switch ($taxonomy):
-        case 'product_cat':
-            $taxonomy_slug = 'product-category'; //Thay bằng slug hiện tại của bạn. Mặc định là product-category
-            if(strpos($url, $taxonomy_slug) === FALSE) break;
-            $url = str_replace('/' . $taxonomy_slug, '', $url);
-            break;
-        case 'thuong-hieu':
-              $taxonomy_slug = 'thuong-hieu'; //Thay bằng slug hiện tại của bạn. Mặc định là thuong-hieu
-              if(strpos($url, $taxonomy_slug) === FALSE) break;
-              $url = str_replace('/' . $taxonomy_slug, '', $url);
-              break;
-    endswitch;
-    return $url;
-}
-// Add our custom product cat rewrite rules
-function devvn_product_category_rewrite_rules($flash = false) {
-    $terms = get_terms( array(
-        'taxonomy' => 'product_cat',
-        'post_type' => 'product',
-        'hide_empty' => false,
-    ));
-    if($terms && !is_wp_error($terms)){
-        $siteurl = esc_url(home_url('/'));
-        foreach ($terms as $term){
-            $term_slug = $term->slug;
-            $baseterm = str_replace($siteurl,'',get_term_link($term->term_id,'product_cat'));
-            add_rewrite_rule($baseterm.'?$','index.php?product_cat='.$term_slug,'top');
-            add_rewrite_rule($baseterm.'page/([0-9]{1,})/?$', 'index.php?product_cat='.$term_slug.'&paged=$matches[1]','top');
-            add_rewrite_rule($baseterm.'(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?product_cat='.$term_slug.'&feed=$matches[1]','top');
-        }
-    }
-    if ($flash == true)
-        flush_rewrite_rules(false);
-}
-add_action('init', 'devvn_product_category_rewrite_rules');
-
-// Add our custom thuong-hieu rewrite rules
-function devvn_thuong_hieu_rewrite_rules($flash = false) {
-  $terms = get_terms( array(
-      'taxonomy' => 'thuong-hieu',
-      'post_type' => 'product',
-      'hide_empty' => false,
-  ));
-  if($terms && !is_wp_error($terms)){
-      $siteurl = esc_url(home_url('/'));
-      foreach ($terms as $term){
-          $term_slug = $term->slug;
-          $baseterm = str_replace($siteurl,'',get_term_link($term->term_id,'thuong-hieu'));
-          add_rewrite_rule($baseterm.'?$','index.php?thuong-hieu='.$term_slug,'top');
-          add_rewrite_rule($baseterm.'page/([0-9]{1,})/?$', 'index.php?thuong-hieu='.$term_slug.'&paged=$matches[1]','top');
-          add_rewrite_rule($baseterm.'(?:feed/)?(feed|rdf|rss|rss2|atom)/?$', 'index.php?thuong-hieu='.$term_slug.'&feed=$matches[1]','top');
-      }
-  }
-  if ($flash == true)
-      flush_rewrite_rules(false);
-}
-add_action('init', 'devvn_thuong_hieu_rewrite_rules');
-/*Sửa lỗi khi tạo mới taxomony bị 404*/
-add_action( 'create_term', 'devvn_new_product_cat_edit_success', 10, 2 );
-function devvn_new_product_cat_edit_success( $term_id, $taxonomy ) {
-    devvn_product_category_rewrite_rules(true);
-    devvn_thuong_hieu_rewrite_rules(true);
-}
