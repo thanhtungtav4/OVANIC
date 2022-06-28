fe56b9e0204aa809df2b2d7c7d4f16633afbd5f5
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-06-28 13:14:08 +0700

    tungnt

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 https://ampforwp.com/tutorials/article/how-to-override-a-template-file-in-amp/
 http://hookr.io/all/
 https://rudrastyh.com/wordpress/remove-taxonomy-slug-from-urls.html
+https://www.shellhacks.com/telegram-api-send-message-personal-notification-bot/
---
+++ wp-content/themes/flatsome-child/functions/telegram.php
@@ -0,0 +1,32 @@
+<?php
+/***
+ * send noti if new order
+ * https://www.shellhacks.com/telegram-api-send-message-personal-notification-bot/
+ */
+add_action('woocommerce_checkout_order_processed', 'notfi_telegram');
+function notfi_telegram($order_id) {
+  if(!$order_id) return;
+  $order = wc_get_order($order_id);
+  $order_data = $order->get_data();
+  $first_name = $order_data['billing']['first_name'];
+  $last_name = $order_data['billing']['last_name'];
+  $phone = $order_data['billing']['phone'];
+  $email = $order_data['billing']['email'];
+  $msg = "<b>Đơn hàng mới </b>: Đơn hàng số [$order_id]  -  <b>Từ khách Hàng </b>: $last_name $first_name - $phone - $email";
+  $botToken= '5440381056:AAGG0St5-y8cVBItKBHl2JHAAqu9pVbqDFY';
+  $website = "https://api.telegram.org/bot".$botToken;
+  $chatId = '1334149635';
+  $params=[
+      'chat_id'=>$chatId,
+      'text'=>$msg,
+      'parse_mode'=>'html',
+  ];
+  $ch = curl_init($website . '/sendMessage');
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  $result = curl_exec($ch);
+  curl_close($ch);
+}
