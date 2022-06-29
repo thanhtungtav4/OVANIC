988ddd1078703b29cdd75041d84291641b046d0a
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-06-29 17:14:41 +0700

    tungnt

--- wp-content/themes/flatsome-child/functions/lang.php
+++ wp-content/themes/flatsome-child/functions/lang.php
@@ -1,1 +1,15 @@
 <?php
+function my_custom_translations( $strings ) {
+  $text = array(
+  'Quick View' => 'Xem nhanh',
+  'SHOPPING CART' => 'Giỏ hàng',
+  'CHECKOUT DETAILS' => 'Thanh toán',
+  'ORDER COMPLETE' => 'Hoàn thành',
+  'Accept' => 'Chấp nhận',
+  'Add to cart' => 'Thêm vào giỏ hàng'
+
+  );
+  $strings = str_ireplace( array_keys( $text ), $text, $strings );
+  return $strings;
+  }
+  add_filter( 'gettext', 'my_custom_translations', 20 );
