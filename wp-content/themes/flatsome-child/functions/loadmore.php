fe99891ca04543254127535a65843a4fcbea804e
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-07-04 12:55:53 +0700

    tungntt

--- wp-content/themes/flatsome-child/functions/loadmore.php
+++ wp-content/themes/flatsome-child/functions/loadmore.php
@@ -6,7 +6,47 @@ add_action('wp_footer','devvn_readmore_flatsome');
 function devvn_readmore_flatsome(){
     ?>
     <style>
-        .single-product div#tab-description{overflow: hidden; position: relative;}.category-page-row .shop-container div.term-description p{overflow: hidden; position: relative;}.single-product .tab-panels div#tab-description.panel:not(.active){height: 0 !important;}.devvn_readmore_flatsome{text-align: center; cursor: pointer; position: absolute; z-index: 9999; bottom: 0; width: 100%; background: #fff;}.devvn_readmore_flatsome:before{height: 55px; margin-top: -45px; content: -webkit-gradient(linear,0% 100%,0% 0%,from(#fff),color-stop(.2,#fff),to(rgba(255,255,255,0))); display: block;}.devvn_readmore_flatsome a{color: #318A00; display: block;}.devvn_readmore_flatsome a:after{content: ''; width: 0; right: 0; border-top: 6px solid #318A00; border-left: 6px solid transparent; border-right: 6px solid transparent; display: inline-block; vertical-align: middle; margin: -2px 0 0 5px;}
+        .single-product div#tab-description {
+            overflow: hidden;
+            position: relative;
+        }
+        .category-page-row .shop-container div.term-description{
+            overflow: hidden;
+            position: relative;
+        }
+        .single-product .tab-panels div#tab-description.panel:not(.active) {
+            height: 0 !important;
+        }
+        .devvn_readmore_flatsome {
+            text-align: center;
+            cursor: pointer;
+            position: absolute;
+            z-index: 9999;
+            bottom: 0;
+            width: 100%;
+            background: #fff;
+        }
+        .devvn_readmore_flatsome:before {
+            height: 55px;
+            margin-top: -45px;
+            content: -webkit-gradient(linear,0% 100%,0% 0%,from(#fff),color-stop(.2,#fff),to(rgba(255,255,255,0)));
+            display: block;
+        }
+        .devvn_readmore_flatsome a {
+            color: #318A00;
+            display: block;
+        }
+        .devvn_readmore_flatsome a:after {
+            content: '';
+            width: 0;
+            right: 0;
+            border-top: 6px solid #318A00;
+            border-left: 6px solid transparent;
+            border-right: 6px solid transparent;
+            display: inline-block;
+            vertical-align: middle;
+            margin: -2px 0 0 5px;
+        }
     </style>
     <script>
         (function($){
@@ -27,8 +67,8 @@ function devvn_readmore_flatsome(){
                             });
                         }
                     }
-                    if($('.category-page-row .shop-container div.term-description p').length > 0){
-                        var wrap = $('.category-page-row .shop-container div.term-description p');
+                    if($('.category-page-row .shop-container div.term-description').length > 0){
+                        var wrap = $('.category-page-row .shop-container div.term-description');
                         var current_height = wrap.height();
                         var your_height = 100;
                         if(current_height > your_height){
