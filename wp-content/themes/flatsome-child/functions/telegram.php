f4c59ef22a256732f9474a32197d952f567cfcaf
tungnt-icd-vn <78336277+tungnt-icd-vn@users.noreply.github.com>
2022-08-18 10:48:14 +0700

    tungnt

--- wp-content/themes/flatsome-child/functions/telegram.php
+++ wp-content/themes/flatsome-child/functions/telegram.php
@@ -31,3 +31,85 @@ function notfi_telegram($order_id) {
   $result = curl_exec($ch);
   curl_close($ch);
 }
+////////////////////
+// is singer product send submit contact
+////////////////////
+
+// add ajax submit form
+function ajax_contact_javascript() {
+  if (is_product ()) {
+    ?>
+        <script type="text/javascript">
+          jQuery( document ).ready( function( $ ) {
+              "use strict";
+              /**
+               * The file is enqueued from inc/admin/class-admin.php.
+              */
+              $( '#product-contact-form' ).submit( function( event ) {
+
+                  event.preventDefault(); // Prevent the default form submit.
+
+                  // serialize the form data
+                  var ajax_form_data = $("#product-contact-form").serialize();
+
+                  //add our own ajax check as X-Requested-With is not always reliable
+                  ajax_form_data = ajax_form_data+'&ajaxrequest=true&submit=Submit+Form';
+
+                  $.ajax({
+                      url:    '/wp-admin/admin-ajax.php', // domain/wp-admin/admin-ajax.php
+                      type:   'post',
+                      data:   ajax_form_data,
+                      action: 'contact_telegram_hook'
+                  })
+
+                  .done( function( response ) { // response from the PHP action
+                      $(" #nds_form_feedback ").html( "The request was successful");
+                  })
+
+                  // something went wrong
+                  .fail( function() {
+                      $(" #nds_form_feedback ").html( "Something went wrong." );
+                  })
+
+                  // after all this time?
+                  .always( function() {
+                      event.target.reset();
+                  });
+              });
+            });
+        </script>
+    <?php
+  }
+}
+add_action('wp_footer', 'ajax_contact_javascript');
+// control request submit to function
+add_action("wp_ajax_contact_telegram_hook", "contact_telegram");
+add_action("wp_ajax_nopriv_contact_telegram_hook", "contact_telegram");
+function contact_telegram(){
+  if(!empty($_POST['ajaxrequest'])){
+    $fullname = $_POST['fullname'] ? $_POST['fullname'] : null;
+    $phone = $_POST['numberphone'] ? $_POST['numberphone'] : null;
+    $product_title = $_POST['product_title'] ? $_POST['product_title'] : null;
+    // in send telegram
+    $msg = "<b>Yêu cầu tư vấn </b>: Từ khách hàng [$fullname]\n <b>Có số điện thoại </b>: [$phone]\n<b>Cho sản phẩm : </b> [$product_title] ";
+    $botToken= '5440381056:AAGG0St5-y8cVBItKBHl2JHAAqu9pVbqDFY';
+    $website = "https://api.telegram.org/bot".$botToken;
+    $chatId = '-737404282';
+    $params=[
+        'chat_id'=>$chatId,
+        'text'=>$msg,
+        'parse_mode'=>'html',
+        'type'=>'group'
+    ];
+    $ch = curl_init($website . '/sendMessage');
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ($params));
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    // !in send telegram
+  }
+
+}
--- wp-content/themes/flatsome-child/module/search_taget.php
+++ wp-content/themes/flatsome-child/module/search_taget.php
@@ -1,11 +1,13 @@
 <div class="detail_info seach_taget">
   <smail>Để lại thông tin để được chúng tôi tư vấn trong thời gian nhanh nhất</smail>
-  <form  class="form-group w-100" name="form" action="" method="get">
-    <input name="fullname" type="text" class="form-control" placeholder="Họ tên">
-    <input name="numberphone" type="text" class="form-control" placeholder="Số điện thoại" pattern="(\+84|0){1}(9|8|7|5|3){1}[0-9]{8}">
+  <form  class="form-group w-100" id="product-contact-form" name="form" method="post">
+    <input name="fullname" id="fullname" type="text" class="form-control" placeholder="Họ tên" required>
+    <input name="numberphone" id="numberphone" type="text" class="form-control" placeholder="Số điện thoại" pattern="(\+84|0){1}(9|8|7|5|3){1}[0-9]{8}" required>
     <input name="product_title" type="hidden" value="<?php the_title(); ?>">
-    <button id="btn-support-single" class="btn btn-submit" type="submit">Nhận tư vấn</button>
+    <input name="action" type="hidden" value="contact_telegram_hook">
+    <button class="btn btn-submit" type="submit">Nhận tư vấn</button>
   </form >
+  <smail id="nds_form_feedback"></smail>
 </div>
 <p class="detail_info seach_taget">
   🔎 Tìm trên Google <strong>"Tên sản phẩm + Ovanic"</strong> để mua sản phẩm chính hãng nhanh nhất nhé!
