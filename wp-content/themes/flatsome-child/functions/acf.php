a67ac041d19296d3abf218c8987514ebd1de62c5
tungnt <nguyenthanhtung74119@gmail.com>
2022-08-28 01:27:42 +0700

    tungnt

--- wp-content/themes/flatsome-child/functions/acf.php
+++ wp-content/themes/flatsome-child/functions/acf.php
@@ -436,7 +436,7 @@ if( function_exists('acf_add_local_field_group') ):
         ),
         'role' => '',
         'allow_null' => 0,
-        'multiple' => 0,
+        'multiple' => 1,
         'return_format' => 'id',
       ),
     ),
--- wp-content/themes/flatsome-child/template-parts/posts/partials/entry-title.php
+++ wp-content/themes/flatsome-child/template-parts/posts/partials/entry-title.php
@@ -21,22 +21,36 @@ if ( $single_post && get_theme_mod( 'blog_single_header_meta', 1 ) ) : ?>
 			<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author external" title="<?php echo get_the_author(); ?>"><?php echo get_the_author(); ?></a>
 		</p>
 		<?php if(get_field('tham_van_y_khoa')) :
-					$id_user = get_field('tham_van_y_khoa');
-					$name_user = get_user_by('id', $id_user)->data->display_name;
-					$url_user = get_author_posts_url($id_user);
-			?>
+			$id_user = get_field('tham_van_y_khoa');
+		?>
 		<p>
 			Tham vấn y khoa -
-			<a href="<?php echo $url_user ?>" rel="author external" title="<?php echo $name_user; ?>">
-			 <?php echo $name_user; ?>
-			</a>
+			<?php foreach($id_user as  $key => $user_item) : ?>
+				<?php 
+					$name_user = get_user_by('id', $user_item)->data->display_name;
+					$url_user = get_author_posts_url($user_item);	
+				?>
+				<a href="<?php echo $url_user ?>" rel="author external" title="<?php echo $name_user; ?>">
+				<?php echo $name_user; ?>
+				</a>
+				<?php 
+					if ($key === array_key_last($id_user)) {
+						echo '.';
+					}
+					else{
+						echo ',';
+					}
+				?>
+			<?php endforeach ; ?>	
 		</p>
+		
+		<?php //flatsome_posted_on(); ?>
+	
+	<?php endif; ?>
 		<p>
 			- <?php echo get_the_date() ?>
 		</p>
-		<?php //flatsome_posted_on(); ?>
 	</div>
-	<?php endif; ?>
 <?php elseif ( ! $single_post && 'post' == get_post_type() ) : ?>
 	<div class="entry-meta uppercase is-xsmall">
 		<?php flatsome_posted_on(); ?>
