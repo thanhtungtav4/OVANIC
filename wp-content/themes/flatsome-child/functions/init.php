8c6d3aca1f40e33b3a2d0d57f9d17fd68bcbc4a3
tungnt <nguyenthanhtung74119@gmail.com>
2022-09-15 00:17:39 +0700

    tungng

--- wp-content/themes/flatsome-child/functions/init.php
+++ wp-content/themes/flatsome-child/functions/init.php
@@ -15,3 +15,29 @@ add_filter('body_class','my_class_names');
 function my_class_names($classes) {
     return array();
 }
+function add_script_fix_devgg(){ ?>
+    <script>
+ 	jQuery.event.special.touchstart = {
+		setup: function( _, ns, handle ) {
+			this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+		}
+	};
+	jQuery.event.special.touchmove = {
+		setup: function( _, ns, handle ) {
+			this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+		}
+	};
+	jQuery.event.special.wheel = {
+		setup: function( _, ns, handle ){
+			this.addEventListener("wheel", handle, { passive: true });
+		}
+	};
+	jQuery.event.special.mousewheel = {
+		setup: function( _, ns, handle ){
+			this.addEventListener("mousewheel", handle, { passive: true });
+		}
+	};
+    </script>
+  <?php 
+}
+add_action('wp_footer', 'add_script_fix_devgg');
